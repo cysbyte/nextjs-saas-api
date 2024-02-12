63524fc95033694f5e0d954d5a0fad27c39ef7e6
chenyunshu2 <chenyunshu2@gmail.com>
2024-02-12 22:53:09 +0800

    adjust padding for wrapper

--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="max-w-screen-xl mx-auto px-10">{children}</div>;
+  return <div className="max-w-screen-xl mx-auto px-20">{children}</div>;
 };
 
 export default Wrapper;
