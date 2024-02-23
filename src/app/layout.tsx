5c46f972dc27c46418ddf0e408b083814f675022
chenyunshu2 <chenyunshu2@gmail.com>
2024-02-24 01:03:24 +0800

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import RecordModal from "@/components/shared/RecordModal";
 import { useEffect } from "react";
+import favicon from './favicon.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+      <link rel="shortcut icon" href={favicon.src} />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
