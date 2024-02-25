a0c784bc151fd55808910eea438376e86833caf9
chenyunshu2 <chenyunshu2@gmail.com>
2024-02-26 04:07:41 +0800

    fix bug

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client";
 
-declare global {
-	var prisma: PrismaClient | undefined;
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
-
-const prisma = global.prisma || new PrismaClient({ log: ["info"] });
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
