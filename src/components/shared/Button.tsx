3e7b0c6ea3eef227c70ed24a8aa896f9649f33f4
chenyunshu2 <chenyunshu2@gmail.com>
2024-02-28 15:57:00 +0800

    update home page

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React, { FC } from "react";
 
 const Button: FC<{ text: string }> = (props) => {
   return (
-    <button className="hover:shadow-lg text-lg font-normal z-0 hover:scale-105 duration-300 active:scale-100 mx-auto w-fit px-5 py-2 bg-indigo-600 rounded-md text-white">
+    <Link href='/product/text-to-speech' className="hover:shadow-lg text-lg font-normal z-0 hover:scale-105 duration-300 active:scale-100 mx-auto w-fit px-5 py-2 bg-indigo-600 rounded-md text-white">
       {props.text}
-    </button>
+    </Link>
   );
 };
 
