769b4fbb21f31cf586e28f80e97341360824abef
chenyunshu2 <chenyunshu2@gmail.com>
2024-03-02 23:16:47 +0800

    fix bug

--- src/app/product/text-to-speech/voiceId/[voiceId]/page.tsx
+++ src/app/product/text-to-speech/voiceId/[voiceId]/page.tsx
@@ -1,11 +1,8 @@
-import Header from "@/components/layout/Header";
-import PriceingPlan from "@/components/sections/price/PriceingPlan";
 import ProductSideBar from "@/components/layout/ProductSideBar";
-import React from "react";
 import Case from "@/components/sections/product/text-to-speech";
-import { loginIsRequiredServer } from "@/lib/auth";
+import { authConfig, loginIsRequiredServer } from "@/lib/auth";
 import prisma from "@/lib/prismadb";
-import { createCustomerIfNull } from "@/lib/stripe";
+import { getServerSession } from "next-auth";
 
 const TextToSpeech = async ({
   params,
@@ -15,32 +12,41 @@ const TextToSpeech = async ({
   };
 }) => {
   await loginIsRequiredServer();
-  //await createCustomerIfNull();
 
+  const session = await getServerSession(authConfig);
+  if (!session) return;
+  console.log(session?.user?.email?.toString());
+  // console.log(session)
   //@ts-ignore
-  // const customVoiceId = await prisma.customVoiceId.findFirst({
-  //     where: {
-  //         id:params.voiceId
-  //     }
-  // }
-  // )
-  // console.log(customVoiceId)
+  const user = await prisma.user.findFirst({
+    where: {
+      email: session?.user?.email?.toString(),
+    },
+  });
 
-  // //@ts-ignore
-  // const voiceNames = await prisma.TextToSpeech.findUnique({
-  //     where: {
-  //         id: params.id
-  //     },
-  //     select: {
-  //         voiceName: true
-  //     }
-  // })
-  // console.log('voiceNames', voiceNames)
+  console.log("user----", user);
+
+  const speechCount = await prisma.textToSpeech.count();
+  const voiceNames = await prisma.textToSpeech.findMany({
+    where: {
+      authorId: user?.id.toString(),
+    },
+    select: {
+      voiceId: true,
+      voiceName: true,
+      mp3_url: true,
+    },
+  });
 
   return (
     <main className="flex flex-row">
       <ProductSideBar productName="TextToSpeech" />
-      <Case voiceId={params.voiceId} />
+      <Case
+        voiceId=""
+        speechCount={speechCount}
+        voiceNames={undefined}
+        user={user}
+      />
     </main>
   );
 };
