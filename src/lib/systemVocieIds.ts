74d59dc51dac49e91fdb53753524fc48bed2eb01
chenyunshu2 <chenyunshu2@gmail.com>
2024-03-03 14:46:01 +0800

    add max durtion config

--- src/lib/systemVocieIds.ts
+++ src/lib/systemVocieIds.ts
@@ -6,43 +6,77 @@ export const systemVoices = [
       "https://saas-minimax.s3.ap-northeast-1.amazonaws.com/text-to-speech/5008eb18-2edd-4593-a8cd-16249db1b8c0.mp3",
   },
 
-  { voiceName: "精英青年音色", voiceId: "male-qn-jingying", audioUrl: '' },
+  {
+    voiceName: "精英青年音色",
+    voiceId: "male-qn-jingying",
+    audioUrl:
+      "https://saas-minimax.s3.ap-northeast-1.amazonaws.com/text-to-speech/b02b90a1-ccd0-4b0c-b381-a361e01f35eb.mp3",
+  },
 
-  { voiceName: "霸道青年音色", voiceId: "male-qn-badao", audioUrl: '' },
+  {
+    voiceName: "霸道青年音色",
+    voiceId: "male-qn-badao",
+    audioUrl:
+      "https://saas-minimax.s3.ap-northeast-1.amazonaws.com/text-to-speech/4c847fdb-9118-414a-a87e-3ce4636ea8c8.mp3",
+  },
 
-  { voiceName: "青年大学生音色", voiceId: "male-qn-daxuesheng", audioUrl: '' },
+  { voiceName: "青年大学生音色", voiceId: "male-qn-daxuesheng", audioUrl: "" },
 
-  { voiceName: "少女音色", voiceId: "female-shaonv", audioUrl: '' },
+  { voiceName: "少女音色", voiceId: "female-shaonv", audioUrl: "" },
 
-  { voiceName: "御姐音色", voiceId: "female-yujie", audioUrl: '' },
+  { voiceName: "御姐音色", voiceId: "female-yujie", audioUrl: "" },
 
-  { voiceName: "成熟女性音色", voiceId: "female-chengshu", audioUrl: '' },
+  { voiceName: "成熟女性音色", voiceId: "female-chengshu", audioUrl: "" },
 
-  { voiceName: "甜美女性音色", voiceId: "female-tianmei", audioUrl: '' },
+  { voiceName: "甜美女性音色", voiceId: "female-tianmei", audioUrl: "" },
 
-  { voiceName: "男性主持人", voiceId: "presenter_male", audioUrl: '' },
+  { voiceName: "男性主持人", voiceId: "presenter_male", audioUrl: "" },
 
-  { voiceName: "女性主持人", voiceId: "presenter_female", audioUrl: '' },
+  { voiceName: "女性主持人", voiceId: "presenter_female", audioUrl: "" },
 
-  { voiceName: "男性有声书1", voiceId: "audiobook_male_1", audioUrl: '' },
+  { voiceName: "男性有声书1", voiceId: "audiobook_male_1", audioUrl: "" },
 
-  { voiceName: "男性有声书2", voiceId: "audiobook_male_2", audioUrl: '' },
+  { voiceName: "男性有声书2", voiceId: "audiobook_male_2", audioUrl: "" },
 
-  { voiceName: "女性有声书1", voiceId: "audiobook_female_1", audioUrl: '' },
+  { voiceName: "女性有声书1", voiceId: "audiobook_female_1", audioUrl: "" },
 
-  { voiceName: "女性有声书2", voiceId: "audiobook_female_2", audioUrl: '' },
+  { voiceName: "女性有声书2", voiceId: "audiobook_female_2", audioUrl: "" },
 
-  { voiceName: "青涩青年音色-beta", voiceId: "male-qn-qingse-jingpin", audioUrl: '' },
+  {
+    voiceName: "青涩青年音色-beta",
+    voiceId: "male-qn-qingse-jingpin",
+    audioUrl: "",
+  },
 
-  { voiceName: "精英青年音色-beta", voiceId: "male-qn-jingying-jingpin", audioUrl: '' },
+  {
+    voiceName: "精英青年音色-beta",
+    voiceId: "male-qn-jingying-jingpin",
+    audioUrl: "",
+  },
 
-    { voiceName: "霸道青年音色 - beta", voiceId: "male - qn - badao - jingpin", audioUrl: '' },
-  
-  { voiceName: "少女音色-beta", voiceId: "female-shaonv-jingpin", audioUrl: '' },
+  {
+    voiceName: "霸道青年音色 - beta",
+    voiceId: "male - qn - badao - jingpin",
+    audioUrl: "",
+  },
 
-  { voiceName: "御姐音色-beta", voiceId: "female-yujie-jingpin", audioUrl: '' },
+  {
+    voiceName: "少女音色-beta",
+    voiceId: "female-shaonv-jingpin",
+    audioUrl: "",
+  },
 
-  { voiceName: "成熟女性音色-beta", voiceId: "female-chengshu-jingpin", audioUrl: '' },
+  { voiceName: "御姐音色-beta", voiceId: "female-yujie-jingpin", audioUrl: "" },
 
-  { voiceName: "甜美女性音色-beta", voiceId: "female-tianmei-jingpin", audioUrl: '' },
+  {
+    voiceName: "成熟女性音色-beta",
+    voiceId: "female-chengshu-jingpin",
+    audioUrl: "",
+  },
+
+  {
+    voiceName: "甜美女性音色-beta",
+    voiceId: "female-tianmei-jingpin",
+    audioUrl: "",
+  },
 ];
