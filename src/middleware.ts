cbcac93671b4c4301ed5bc23a9fc5e85953c9ffb
chenyunshu2 <chenyunshu2@gmail.com>
2024-07-09 19:19:48 +0800

    fixed: for vercel connect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
     const response = NextResponse.next();
 
-    if (request.nextUrl.pathname === "/job") {
-        return NextResponse.rewrite(new URL("/jobs", request.url));
-      }
-
     const themePreference = request.cookies.get('theme')
     if (!themePreference) {
         response.cookies.set('theme', 'dark')
